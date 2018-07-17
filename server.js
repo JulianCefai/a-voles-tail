344d2038e577d63a74dd0228bb369b4b730c85ae
Julian Cefai <jcjcefai@gmail.com>
2018-07-18 00:21:00 +0100

    First Server Trial For Voles Tail App

---
+++ server.js
@@ -0,0 +1,32 @@
+//Require express package for networking and filesystem module
+const express = require('express');
+const fs = require('fs')
+//Make  a new express app. Config done later, no arguments passed to express
+var app = express()
+
+//Add middleware to create static directory with multimedia to serve. Pass absolute file path to public
+app.use(express.static(__dirname + '/public'));
+
+//Middleware to log times of HTTP requests
+
+app.use((req,res,next) => {
+var now = new Date().toString();
+var path = '${req.url}'
+var log = '${now}: ${req.method} ${req.url}';
+fs.appendFile('server.log', log + '\n',(err) => {
+  if (err){
+
+  }
+});
+next();
+});
+
+
+//HTTP Route Handlers Registration
+//Pass URL and function to run to get as args. In this case arrow function with two arguments: request(storing info about incoming request), response can be string, JSON, etc. In this case will be URL
+app.get('/',(req,res) => {
+  res.send('HTTP Response String')
+});
+
+//Start Listening at port 3000 for local dev
+app.listen(3000);
